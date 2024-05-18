87f986067dd8ec81bd855434bceb34d7a0dd8d72
EronAlves1996 <eron.alves@rocketmail.com>
2024-05-17 23:46:32 -0300

    feat: include config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { IntegrationsController } from './integrations/integrations.controller';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController, IntegrationsController],
   providers: [AppService],
 })
