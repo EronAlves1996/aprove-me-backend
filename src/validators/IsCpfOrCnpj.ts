d5253b1464c195575534c615d189e72d9d123233
EronAlves1996 <eron.alves@rocketmail.com>
2024-05-18 00:25:56 -0300

    feat: create custom validator

---
+++ src/validators/IsCpfOrCnpj.ts
@@ -0,0 +1,21 @@
+import { ValidationOptions, registerDecorator } from 'class-validator';
+
+export function IsCpfOrCnpj(validationOptions?: ValidationOptions) {
+  return function (object: Object, propertyName: string) {
+    registerDecorator({
+      name: 'IsCpfOrCnpj',
+      target: object.constructor,
+      propertyName: propertyName,
+      options: validationOptions,
+      validator: {
+        validate: (value, _) => {
+          if (typeof value !== 'string') {
+            return false;
+          }
+          const stripped = value.replace(/\D/g, '');
+          return stripped.length === 11 || stripped.length === 14;
+        },
+      },
+    });
+  };
+}
