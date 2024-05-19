1ac2f7068cd0e85d190c36694c5a05be11dd4e0d
EronAlves1996 <eron.alves@rocketmail.com>
2024-05-18 23:28:40 -0300

    fix: server not running

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { IntegrationsController } from './integrations/payable.controller';
+import { AssignorController } from './integrations/assignor.controller';
+import { PayableController } from './integrations/payable.controller';
+import { PayableService } from './services/payable.service';
+import { AssignorService } from './services/assignor.service';
+import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [ConfigModule.forRoot()],
-  controllers: [AppController, IntegrationsController],
-  providers: [AppService],
+  controllers: [AppController, PayableController, AssignorController],
+  providers: [AppService, PayableService, AssignorService, PrismaService],
 })
 export class AppModule {}
