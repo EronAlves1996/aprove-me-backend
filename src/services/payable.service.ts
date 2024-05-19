51cdc62183f9f52221404de595ef83eea4663dd7
EronAlves1996 <eron.alves@rocketmail.com>
2024-05-19 12:45:41 -0300

    feat: create crud operations payable

--- src/services/payable.service.ts
+++ src/services/payable.service.ts
@@ -6,13 +6,31 @@ import { PrismaService } from 'src/prisma.service';
 export class PayableService {
   constructor(private prisma: PrismaService) {}
 
-  async create(data: Prisma.PayableCreateInput): Promise<Payable> {
+  async exists(where: Prisma.PayableWhereUniqueInput): Promise<boolean> {
+    return (await this.prisma.payable.count({ where })) > 0;
+  }
+
+  retrieve(where: Prisma.PayableWhereUniqueInput): Promise<Payable> {
+    return this.prisma.payable.findFirst({ where });
+  }
+
+  create(data: Prisma.PayableCreateInput): Promise<Payable> {
     return this.prisma.payable.create({
       data,
     });
   }
 
-  async retrieveById(id: string): Promise<Payable> {
-    return this.prisma.payable.findFirst({ where: { id } });
+  async update({
+    data,
+    where,
+  }: {
+    data: Prisma.PayableCreateInput;
+    where: Prisma.PayableWhereUniqueInput;
+  }): Promise<void> {
+    this.prisma.payable.update({ data, where });
+  }
+
+  async delete(where: Prisma.PayableWhereUniqueInput) {
+    this.prisma.payable.delete({ where });
   }
 }
