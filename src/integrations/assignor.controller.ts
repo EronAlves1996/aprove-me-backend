c5e0d8e815ef3b6d79032ca35a0b8e3a41795540
EronAlves1996 <eron.alves@rocketmail.com>
2024-05-19 12:59:00 -0300

    feat: expose crud operations assignor

--- src/integrations/assignor.controller.ts
+++ src/integrations/assignor.controller.ts
@@ -1,4 +1,14 @@
-import { Body, Controller, Get, Param, Post, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+  Res,
+} from '@nestjs/common';
 import { Response } from 'express';
 import { AssignorDTO } from 'src/dtos/AssignorDTO';
 import { AssignorService } from 'src/services/assignor.service';
@@ -9,7 +19,7 @@ export class AssignorController {
 
   @Get('/:id')
   async retrieve(@Param() { id }: { id: string }, @Res() response: Response) {
-    const assignor = await this.assignorService.retrieveById(id);
+    const assignor = await this.assignorService.retrieve({ id });
     if (!assignor) {
       response.statusCode = 404;
       response.json({ message: 'Not Found' });
@@ -24,5 +34,34 @@ export class AssignorController {
     response.setHeader('location', `/integrations/payable/${id}`);
     response.statusCode = 201;
     response.send();
+  }
+
+  @Put('/:id')
+  async updateOrCreate(
+    @Param() { id }: { id: string },
+    @Body() assignorDTO: AssignorDTO,
+    @Res() response: Response,
+  ) {
+    if (id !== assignorDTO.id) {
+      response.statusCode = HttpStatus.BAD_REQUEST;
+      response.json({
+        message:
+          'Operation permitted only on entity specified in path param. Verify your input and try again',
+      });
+      return;
+    }
+
+    if (await this.assignorService.exists({ id })) {
+      await this.assignorService.update({ data: assignorDTO, where: { id } });
+      response.send();
+      return;
+    }
+
+    await this.create(assignorDTO, response);
+  }
+
+  @Delete('/:id')
+  async delete(@Param() { id }: { id: string }) {
+    await this.assignorService.delete({ id });
   }
 }
