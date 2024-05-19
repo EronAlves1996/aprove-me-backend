7d997ac87358fdcdec1bd59cf2a099291a997bcc
EronAlves1996 <eron.alves@rocketmail.com>
2024-05-19 13:10:57 -0300

    feat: expose crud operations payable

--- src/integrations/payable.controller.ts
+++ src/integrations/payable.controller.ts
@@ -1,7 +1,18 @@
-import { Body, Controller, Get, Param, Post, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+  Res,
+} from '@nestjs/common';
 import { PayableDTO } from '../dtos/PayableDTO';
 import { PayableService } from 'src/services/payable.service';
 import { Response } from 'express';
+import { ENTITY_ID_NOT_EQUALS } from 'src/messages';
 
 @Controller('integrations/payable')
 export class PayableController {
@@ -9,7 +20,7 @@ export class PayableController {
 
   @Get('/:id')
   async retrieve(@Param() { id }: { id: string }, @Res() response: Response) {
-    const payable = await this.payableService.retrieveById(id);
+    const payable = await this.payableService.retrieve({ id });
     if (!payable) {
       response.statusCode = 404;
       response.json({ message: 'Not Found' });
@@ -34,5 +45,42 @@ export class PayableController {
     response.setHeader('location', `/integrations/payable/${createdId}`);
     response.statusCode = 201;
     response.send();
+  }
+
+  @Put('/:id')
+  async createOrUpdate(
+    @Param() { id }: { id: string },
+    @Body() data: PayableDTO,
+    @Res() response: Response,
+  ) {
+    if (id !== data.id) {
+      response.statusCode = HttpStatus.BAD_REQUEST;
+      response.json(ENTITY_ID_NOT_EQUALS);
+      return;
+    }
+    if (await this.payableService.exists({ id })) {
+      const { assignor, emissionDate, value } = data;
+      await this.payableService.update({
+        where: { id },
+        data: {
+          id,
+          emissionDate,
+          value,
+          assignorData: {
+            connect: {
+              id: assignor,
+            },
+          },
+        },
+      });
+      response.send();
+      return;
+    }
+    await this.create(data, response);
+  }
+
+  @Delete('/:id')
+  async delete(@Param() { id }: { id: string }) {
+    await this.payableService.delete({ id });
   }
 }
